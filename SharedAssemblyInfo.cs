a2fe011664621a2d9ce760e81b0ff1588a23522b
denis-arharov <denis-arharov@yandex.ru>
2015-10-29 04:09:03 +0200

    Moved to version 0.0.0.1

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
 
